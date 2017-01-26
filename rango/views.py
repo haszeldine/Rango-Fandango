e499e4b54b8adc642eabc38cba0d9111c25f0437
Calum Haszeldine <calum.thain@gmail.com>
2017-01-26 11:04:42 +0000

    Added category view and urls to index

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rango.models import Category
+from rango.models import Page
 
 
 def index(request):
@@ -10,3 +11,18 @@ def index(request):
 
 def about(request):
 	return render(request, 'rango/about.html')
+
+
+def show_category(request, category_name_slug):
+	context_dict = {}
+
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		pages = Page.objects.filter(category=category)
+		context_dict['pages'] = pages
+		context_dict['category'] = category
+	except Category.DoesNotExist:
+		context_dict['category'] = None
+		context_dict['pages'] = None
+
+	return render(request, 'rango/category.html', context_dict)
