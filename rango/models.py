1336b0f372ac5165e06e45d5755c653e01e50096
Calum Haszeldine <calum.thain@gmail.com>
2017-02-15 10:53:53 +0000

    Finished 9 - login, logout, restricted, is_authorised

--- rango/models.py
+++ rango/models.py
@@ -1,38 +1,52 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    max_length = 128
-    name = models.CharField(max_length=max_length, unique=True)
-    views = models.IntegerField(default=0)
-    likes = models.IntegerField(default=0)
-    slug = models.SlugField(unique=True)
+	max_length = 128
+	name = models.CharField(max_length=max_length, unique=True)
+	views = models.IntegerField(default=0)
+	likes = models.IntegerField(default=0)
+	slug = models.SlugField(unique=True)
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super(Category, self).save(*args, **kwargs)
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Category, self).save(*args, **kwargs)
 
-    class Meta:
-        verbose_name_plural = 'categories'
+	class Meta:
+		verbose_name_plural = 'categories'
 
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
 
-    def __unicode__(self):
-        return self.name
+	def __unicode__(self):
+		return self.name
 
 
 class Page(models.Model):
-    max_length = 128
-    category = models.ForeignKey(Category)
-    title = models.CharField(max_length=max_length)
-    url = models.URLField()
-    views = models.IntegerField(default=0)
+	max_length = 128
+	category = models.ForeignKey(Category)
+	title = models.CharField(max_length=max_length)
+	url = models.URLField()
+	views = models.IntegerField(default=0)
 
-    def __str__(self):
-        return self.title
+	def __str__(self):
+		return self.title
 
-    def __unicode__(self):
-        return self.title
+	def __unicode__(self):
+		return self.title
+
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+
+	website = models.URLField(blank=True)
+	picture = models.ImageField(upload_to='profile_images', blank=True)
+
+	def __str__(self):
+		return self.user.username
+
+	def __unicode__(self):
+		return self.user.username
